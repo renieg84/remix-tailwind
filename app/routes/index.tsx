f6d975bde9f6f221379f85cfe171fd377fbdd73d
Renier Martinez Guzman <renieg84@gmail.com>
2021-12-13 08:15:03 -0500

    Minor change.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-blue-700 flex justify-center w-full items-center h-screen text-2xl bg-blue-100">
-      Home
+      Home Page
     </div>
   );
 }
